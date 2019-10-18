d6eaa6a12ddbcaa61d72f5da21565e8a134773d7
ErwannLG <e.legall@codeur.online>
2019-10-18 14:11:06 +0200

    enter to search & adding a loading animation

--- Components/Search.js
+++ Components/Search.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
-import FilmItem from './FilmItem'
-import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+import React from 'react';
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native';
+import FilmItem from './FilmItem';
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
 
 class Search extends React.Component {
 
@@ -20,14 +20,30 @@ class Search extends React.Component {
 
   _loadFilms() {
     if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      this.setState({ isLoading: true }) // Lancement du chargement
       getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-          this.setState({ films: data.results })
+          this.setState({
+            films: data.results,
+            isLoading: false // Arrêt du chargement
+          })
       })
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
     }
   }
 
   render() {
     console.log("RENDER")
+    console.log(this.state.isLoading)
     return (
       <View style={styles.main_container}>
         <TextInput
@@ -42,6 +58,7 @@ class Search extends React.Component {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -59,6 +76,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
